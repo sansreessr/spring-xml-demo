9faa78a7ab0aa80efd2efc56ee6d5c46021bb528
sansreessr <sreenathsankarssr@gmail.com>
2019-09-23 15:23:22 +0530

    task 2

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,34 +1,23 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 public class Main
 {
     public static void main( String[] args ) {
 
-        //Using XmlBeanFactory
-        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movie = (Movie) factory.getBean("movie");
-        System.out.println("Using XmlBeanFactory: " + movie.getActor());
-
-        //Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader
-        BeanDefinitionRegistry registry = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        BeanDefinitionReader reader = new XmlBeanDefinitionReader(registry);
-        BeanFactory factory1 = (BeanFactory) reader.getRegistry();
-        Movie movie1 = (Movie) factory1.getBean("movie");
-        System.out.println("Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader: " + movie1.getActor());
-
-        //Using ApplicationContext
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie2 = (Movie) context.getBean("movie");
-        System.out.println("Using ApplicationContext: " + movie2.getActor());
+        Movie movie = (Movie) context.getBean("movie");
+        System.out.println("Actor Info; BeanA: " + movie.getActor());
+
+        Movie movie1 = (Movie) context.getBean("movie");
+        System.out.println("Actor Info; BeanB: " + movie1.getActor());
+
+        System.out.println(movie==movie1);
+
+        Movie movie2 = (Movie) context.getBean("movieB");
+        System.out.println("Actor Info; Second Name: " + movie2.getActor());
     }
 }
--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -6,17 +6,11 @@ public class Actor {
     private String gender;
     private int age;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
+   public Actor(String name, String gender, int age) {
+       this.name = name;
+       this.gender = gender;
+       this.age = age;
+   }
 
     public String getName() {
         return name;
