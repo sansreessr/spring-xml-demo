b8ea21ad6236f69f160828a56d7e64780f8de010
sansreessr <sreenathsankarssr@gmail.com>
2019-09-24 09:19:09 +0530

    task3

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -9,15 +9,10 @@ public class Main
     public static void main( String[] args ) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie = (Movie) context.getBean("movie");
+        Movie movie = (Movie) context.getBean("movie", Movie.class);
         System.out.println("Actor Info; BeanA: " + movie.getActor());
 
-        Movie movie1 = (Movie) context.getBean("movie");
+        Movie movie1 = (Movie) context.getBean("movieB", Movie.class);
         System.out.println("Actor Info; BeanB: " + movie1.getActor());
-
-        System.out.println(movie==movie1);
-
-        Movie movie2 = (Movie) context.getBean("movieB");
-        System.out.println("Actor Info; Second Name: " + movie2.getActor());
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,10 +1,19 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Movie {
 
     private Actor actor;
 
+    public Movie() {}
+
     public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
+    public void setActor(Actor actor) {
         this.actor = actor;
     }
 
